4fdc953b9f5e6ab5ac6eba6b1c4bb16fa969628c
Josh Worden <josh@parameter1.com>
2020-08-27 14:01:17 -0500

    Initial configs

---
+++ tenants/fcp/config/core.js
@@ -0,0 +1,21 @@
+module.exports = {
+  dss: {
+    socialMediaLinks: [
+      {
+        provider: 'facebook',
+        href: 'https://www.facebook.com/fordsspros',
+        title: 'Follow us on Facebook',
+      },
+      {
+        provider: 'twitter',
+        href: 'https://twitter.com/fordsspros',
+        title: 'Follow us on Twitter',
+      },
+      {
+        provider: 'linkedin',
+        href: 'https://www.linkedin.com/company/fordsspros/',
+        title: 'Follow us on LinkedIn',
+      },
+    ],
+  },
+};
---
+++ tenants/fcp/config/email-x.js
@@ -0,0 +1,23 @@
+const EmailXConfiguration = require('@base-cms/marko-newsletters-email-x/config');
+
+const config = new EmailXConfiguration('https://acbm.serve.email-x.io');
+
+config
+  .setAdUnits('openings-news-views', [
+    {
+      name: 'banner-1',
+      id: '5f4802b04c15d921867b2da7',
+      width: 600,
+      height: 100,
+    },
+  ])
+  .setAdUnits('openings-news-views', [
+    {
+      name: 'banner-2',
+      id: '5f4802be4c15d93bce7b2dba',
+      width: 600,
+      height: 100,
+    },
+  ]);
+
+module.exports = config;
