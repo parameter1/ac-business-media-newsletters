6f8f262a9ca176484b966e8cef639f9a0d14829f
Brandon Krigbaum <brandonbk@gmail.com>
2021-06-30 08:20:30 -0500

    Add dpm config

--- tenants/fcp/config/core.js
+++ tenants/fcp/config/core.js
@@ -1,9 +1,15 @@
 const brands = require('./brands');
 const emailX = require('./email-x');
 // const nativeX = require('./native-x');
+
 module.exports = {
   emailX,
   // nativeX,
+  dpm: {
+    emailx: {
+      enabled: true,
+    },
+  },
   dss: {
     socialMediaLinks: [
       {
