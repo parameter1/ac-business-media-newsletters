abb9d76d2b7cd6f51b577e5b89a371f16e8da3fe
Jessica Wade <jessica.wade@parameter1.com>
2021-08-06 14:06:11 -0500

    Create sdc config files

---
+++ tenants/sdc/config/core.js
@@ -0,0 +1,13 @@
+// const brands = require('./brands');
+const emailX = require('./email-x');
+// const nativeX = require('./native-x');
+
+module.exports = {
+  emailX,
+  // nativeX,
+  dpm: {
+    emailx: {
+      enabled: true,
+    },
+  },
+};
---
+++ tenants/sdc/config/socialShortcuts.js
@@ -0,0 +1,8 @@
+/**
+* Used to condense socialMedias.js
+* Used to store all of the different social media links
+*/
+module.exports = {
+  socials: {
+  },
+};
