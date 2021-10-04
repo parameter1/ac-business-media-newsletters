40662b9c838bbfe00b8d3fba1978c40fe952fa98
Jessica Wade <jessica.wade@parameter1.com>
2021-10-04 09:08:01 -0500

    Create snowPro newsletter

--- tenants/gip/config/email-x.js
+++ tenants/gip/config/email-x.js
@@ -1,20 +1,26 @@
-// const EmailXConfiguration = require('@parameter1/base-cms-marko-newsletters-email-x/config');
+const EmailXConfiguration = require('@parameter1/base-cms-marko-newsletters-email-x/config');
 
-// const config = new EmailXConfiguration('https://acbm.serve.email-x.parameter1.com');
+const config = new EmailXConfiguration('https://acbm.serve.email-x.parameter1.com');
 
-// config
-//   .setAdUnits('', [
-//     {
-//       name: 'banner-1',
-//       id: '',
-//       width: 600,
-//       height: 100,
-//     },
-//     {
-//       name: 'banner-2',
-//       id: '',
-//       width: 600,
-//       height: 100,
-//     },
-//   ]);
-// module.exports = config;
+config
+  .setAdUnits('snowpro', [
+    {
+      name: 'banner-1',
+      id: '5f4f9270496c5762f9d20672',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'banner-2',
+      id: '5c59a417465d19615bab84a5',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'banner-3',
+      id: '5e3d906e58868246feaa8b0a',
+      width: 600,
+      height: 100,
+    },
+  ]);
+module.exports = config;
