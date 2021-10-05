f8204e8400026d24412962bf44c4ebd093180e11
Jessica Wade <jessica.wade@parameter1.com>
2021-10-05 09:12:01 -0500

    Update config files for FL newsletter

---
+++ tenants/fl/config/brands.js
@@ -0,0 +1,19 @@
+module.exports = {
+  fl: {
+    brandName: 'Food Logistics',
+    headerLogoSrc: '',
+    footerLogoSrc: '',
+    contactUsLink: 'https://www.foodlogistics.com/contact-us',
+    newsletterPrefLink: 'https://acbusiness.dragonforms.com/loading.do?omedasite=FLprefs&pk=managepref&r',
+    privacyPolicyLink: 'https://www.foodlogistics.com/page/privacy-policy',
+    socialMedia: {
+      imagePath: '/files/base/acbm/fl/image/static/newsletter/',
+      links: [
+        { provider: 'facebook', href: 'https://www.facebook.com/FoodLogistics', target: '_blank' },
+        { provider: 'linkedin', href: 'https://www.linkedin.com/groups/3028432/', target: '_blank' },
+        { provider: 'twitter', href: 'https://twitter.com/FoodLogistics', target: '_blank' },
+        { provider: 'instagram', href: 'https://www.instagram.com/foodlogisticsmag/', target: '_blank' },
+      ],
+    },
+  },
+};
