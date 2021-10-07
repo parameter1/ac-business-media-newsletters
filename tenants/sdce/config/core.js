74b8bba8ab137099379baf73d1c8ab5d0ff3d561
Jessica Wade <jessica.wade@parameter1.com>
2021-10-07 09:35:24 -0500

    Update editor title

--- tenants/sdce/config/core.js
+++ tenants/sdce/config/core.js
@@ -15,7 +15,7 @@ module.exports = {
     name: 'Headline News',
     primaryColor: '#e50102',
     editor: 'Brielle Jaekel',
-    editorTitle: 'Associate Editor',
+    editorTitle: 'Managing Editor',
     editorImage: '/files/base/acbm/sdce/image/static/Brielle.png',
   },
 };
