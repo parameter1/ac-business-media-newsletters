39267203cc8b579663d33f0c7102e95c777a83ff
Jessica Wade <jessica.wade@parameter1.com>
2021-10-13 11:54:04 -0500

    Update brands & core files for FCP templates

--- tenants/fcp/config/brands.js
+++ tenants/fcp/config/brands.js
@@ -1,8 +1,7 @@
 module.exports = {
   fcp: {
     brandName: 'For Construction Pros',
-    headerLogoSrc: '',
-    footerLogoSrc: '',
+    headerLogoSrc: '/files/base/acbm/fcp/image/static/logo/FCP_logo_White.png',
     contactUsLink: 'https://www.forconstructionpros.com/contact-us',
     newsletterPrefLink: 'https://acbusiness.dragonforms.com/loading.do?omedasite=FCP_prefs_ProgReg',
     privacyPolicyLink: 'https://www.forconstructionpros.com/privacy-policy',
@@ -29,92 +28,87 @@ module.exports = {
   //     ],
   //   },
   // },
-  // pavementMagazine: {
-  //   brandName: 'Pavement Management',
-  //   footerLogoSrc: '/files/base/acbm/fcp/image/static/newsletter/footer-fcp.jpeg',
-  //   supportEmail: 'support@forconstructionpros.com',
-  //   contactUsLink: 'https://www.forconstructionpros.com/contact-us',
-  //   newsletterPrefLink: 'https://acbusiness.dragonforms.com/loading.do?omedasite=FCP_prefs_ProgReg',
-  //   privacyPolicyLink: 'https://www.forconstructionpros.com/privacy-policy',
-  //   socialMedia: {
-  //     imagePath: '/files/base/acbm/fcp/image/static/newsletter',
-  //     links: [
-  //       { provider: 'facebook', href: 'https://www.facebook.com/PavementMagazine' },
-  //       { provider: 'linkedin', href: 'https://www.linkedin.com/company/pavement-maintenance-reconstruction-magazine/' },
-  //       { provider: 'twitter', href: 'https://twitter.com/PavementMag' },
-  //       { provider: 'instagram', href: 'https://www.instagram.com/pavementmagazine/' },
-  //     ],
-  //   },
-  // },
-  // equipmentToday: {
-  //   brandName: 'Equipment Today',
-  //   footerLogoSrc: '/files/base/acbm/fcp/image/static/newsletter/footer-fcp.jpeg',
-  //   supportEmail: 'support@forconstructionpros.com',
-  //   contactUsLink: 'https://www.forconstructionpros.com/contact-us',
-  //   newsletterPrefLink: 'https://acbusiness.dragonforms.com/loading.do?omedasite=FCP_prefs_ProgReg',
-  //   privacyPolicyLink: 'https://www.forconstructionpros.com/privacy-policy',
-  //   socialMedia: {
-  //     imagePath: '/files/base/acbm/fcp/image/static/newsletter',
-  //     links: [
-  //       { provider: 'facebook', href: 'https://www.facebook.com/EquipmentToday' },
-  //       { provider: 'linkedin', href: 'https://www.linkedin.com/company/equipment-today-magazine-official/' },
-  //       { provider: 'twitter', href: 'https://twitter.com/EquipmentToday' },
-  //       { provider: 'instagram', href: 'https://www.instagram.com/equipmenttoday/' },
-  //       { provider: 'youtube', href: 'https://www.youtube.com/user/ForConstructionPros' },
-  //     ],
-  //   },
-  // },
-  // rentalMagazine: {
-  //   brandName: 'Rental Magazine',
-  //   footerLogoSrc: '/files/base/acbm/fcp/image/static/newsletter/footer-fcp.jpeg',
-  //   supportEmail: 'support@forconstructionpros.com',
-  //   contactUsLink: 'https://www.forconstructionpros.com/contact-us',
-  //   newsletterPrefLink: 'https://acbusiness.dragonforms.com/loading.do?omedasite=FCP_prefs_ProgReg',
-  //   privacyPolicyLink: 'https://www.forconstructionpros.com/privacy-policy',
-  //   socialMedia: {
-  //     imagePath: '/files/base/acbm/fcp/image/static/newsletter',
-  //     links: [
-  //       { provider: 'facebook', href: 'https://www.facebook.com/RentalMagazine' },
-  //       { provider: 'linkedin', href: 'https://www.linkedin.com/company/rental-magazine/' },
-  //       { provider: 'twitter', href: 'https://twitter.com/RentalPNI' },
-  //       { provider: 'instagram', href: 'https://www.instagram.com/rentalpni/' },
-  //     ],
-  //   },
-  // },
-  // concreteContractor: {
-  //   brandName: 'Concrete Contractor',
-  //   footerLogoSrc: '/files/base/acbm/fcp/image/static/newsletter/footer-fcp.jpeg',
-  //   supportEmail: 'support@forconstructionpros.com',
-  //   contactUsLink: 'https://www.forconstructionpros.com/contact-us',
-  //   newsletterPrefLink: 'https://acbusiness.dragonforms.com/loading.do?omedasite=FCP_prefs_ProgReg',
-  //   privacyPolicyLink: 'https://www.forconstructionpros.com/privacy-policy',
-  //   socialMedia: {
-  //     imagePath: '/files/base/acbm/fcp/image/static/newsletter',
-  //     links: [
-  //       { provider: 'facebook', href: 'https://www.facebook.com/concretecontractor' },
-  //       { provider: 'linkedin', href: 'https://www.linkedin.com/company/concrete-contractor-magazine/' },
-  //       { provider: 'twitter', href: 'https://twitter.com/concreteinsider' },
-  //       { provider: 'instagram', href: 'https://instagram.com/concretecontractor/' },
-  //     ],
-  //   },
-  // },
-  // asphaltContractor: {
-  //   brandName: 'AsphaltContractor',
-  //   footerLogoSrc: '/files/base/acbm/fcp/image/static/newsletter/footer-fcp.jpeg',
-  //   supportEmail: 'support@forconstructionpros.com',
-  //   contactUsLink: 'https://www.forconstructionpros.com/contact-us',
-  //   newsletterPrefLink: 'https://acbusiness.dragonforms.com/loading.do?omedasite=FCP_prefs_ProgReg',
-  //   privacyPolicyLink: 'https://www.forconstructionpros.com/privacy-policy',
-  //   socialMedia: {
-  //     imagePath: '/files/base/acbm/fcp/image/static/newsletter',
-  //     links: [
-  //       { provider: 'facebook', href: 'https://www.facebook.com/Asphalt.Contractor/' },
-  //       { provider: 'linkedin', href: 'https://www.linkedin.com/company/asphalt-contractor-magazine/' },
-  //       { provider: 'twitter', href: 'https://twitter.com/AsphaltContrctr' },
-  //       { provider: 'instagram', href: 'https://www.instagram.com/asphaltcontractor/' },
-  //     ],
-  //   },
-  // },
+  pavementMagazine: {
+    brandName: 'Pavement Management',
+    headerLogoSrc: '/files/base/acbm/fcp/image/static/logo/Pavement_logo.png',
+    contactUsLink: 'https://www.forconstructionpros.com/contact-us',
+    newsletterPrefLink: 'https://acbusiness.dragonforms.com/loading.do?omedasite=FCP_prefs_ProgReg',
+    privacyPolicyLink: 'https://www.forconstructionpros.com/privacy-policy',
+    socialMedia: {
+      imagePath: '/files/base/acbm/fcp/image/static/newsletter',
+      links: [
+        { provider: 'facebook', href: 'https://www.facebook.com/PavementMagazine' },
+        { provider: 'linkedin', href: 'https://www.linkedin.com/company/pavement-maintenance-reconstruction-magazine/' },
+        { provider: 'twitter', href: 'https://twitter.com/PavementMag' },
+        { provider: 'instagram', href: 'https://www.instagram.com/pavementmagazine/' },
+      ],
+    },
+  },
+  equipmentToday: {
+    brandName: 'Equipment Today',
+    headerLogoSrc: '/files/base/acbm/fcp/image/static/logo/EqupToday_logo.png',
+    contactUsLink: 'https://www.forconstructionpros.com/contact-us',
+    newsletterPrefLink: 'https://acbusiness.dragonforms.com/loading.do?omedasite=FCP_prefs_ProgReg',
+    privacyPolicyLink: 'https://www.forconstructionpros.com/privacy-policy',
+    socialMedia: {
+      imagePath: '/files/base/acbm/fcp/image/static/newsletter',
+      links: [
+        { provider: 'facebook', href: 'https://www.facebook.com/EquipmentToday' },
+        { provider: 'linkedin', href: 'https://www.linkedin.com/company/equipment-today-magazine-official/' },
+        { provider: 'twitter', href: 'https://twitter.com/EquipmentToday' },
+        { provider: 'instagram', href: 'https://www.instagram.com/equipmenttoday/' },
+        { provider: 'youtube', href: 'https://www.youtube.com/user/ForConstructionPros' },
+      ],
+    },
+  },
+  rentalMagazine: {
+    brandName: 'Rental Magazine',
+    headerLogoSrc: '/files/base/acbm/fcp/image/static/logo/Rental_logo.png',
+    contactUsLink: 'https://www.forconstructionpros.com/contact-us',
+    newsletterPrefLink: 'https://acbusiness.dragonforms.com/loading.do?omedasite=FCP_prefs_ProgReg',
+    privacyPolicyLink: 'https://www.forconstructionpros.com/privacy-policy',
+    socialMedia: {
+      imagePath: '/files/base/acbm/fcp/image/static/newsletter',
+      links: [
+        { provider: 'facebook', href: 'https://www.facebook.com/RentalMagazine' },
+        { provider: 'linkedin', href: 'https://www.linkedin.com/company/rental-magazine/' },
+        { provider: 'twitter', href: 'https://twitter.com/RentalPNI' },
+        { provider: 'instagram', href: 'https://www.instagram.com/rentalpni/' },
+      ],
+    },
+  },
+  concreteContractor: {
+    brandName: 'Concrete Contractor',
+    headerLogoSrc: '/files/base/acbm/fcp/image/static/logo/ConcreteContractor_logo.png',
+    contactUsLink: 'https://www.forconstructionpros.com/contact-us',
+    newsletterPrefLink: 'https://acbusiness.dragonforms.com/loading.do?omedasite=FCP_prefs_ProgReg',
+    privacyPolicyLink: 'https://www.forconstructionpros.com/privacy-policy',
+    socialMedia: {
+      imagePath: '/files/base/acbm/fcp/image/static/newsletter',
+      links: [
+        { provider: 'facebook', href: 'https://www.facebook.com/concretecontractor' },
+        { provider: 'linkedin', href: 'https://www.linkedin.com/company/concrete-contractor-magazine/' },
+        { provider: 'twitter', href: 'https://twitter.com/concreteinsider' },
+        { provider: 'instagram', href: 'https://instagram.com/concretecontractor/' },
+      ],
+    },
+  },
+  asphaltContractor: {
+    brandName: 'AsphaltContractor',
+    headerLogoSrc: '/files/base/acbm/fcp/image/static/logo/AsphaltContractor_logo.png',
+    contactUsLink: 'https://www.forconstructionpros.com/contact-us',
+    newsletterPrefLink: 'https://acbusiness.dragonforms.com/loading.do?omedasite=FCP_prefs_ProgReg',
+    privacyPolicyLink: 'https://www.forconstructionpros.com/privacy-policy',
+    socialMedia: {
+      imagePath: '/files/base/acbm/fcp/image/static/newsletter',
+      links: [
+        { provider: 'facebook', href: 'https://www.facebook.com/Asphalt.Contractor/' },
+        { provider: 'linkedin', href: 'https://www.linkedin.com/company/asphalt-contractor-magazine/' },
+        { provider: 'twitter', href: 'https://twitter.com/AsphaltContrctr' },
+        { provider: 'instagram', href: 'https://www.instagram.com/asphaltcontractor/' },
+      ],
+    },
+  },
   // psc: {
   //   brandName: 'Product Showcase',
   //   socialMedia: {
