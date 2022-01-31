20dbb849316f2ca0f5946498418e28f93c251933
Josh Worden <josh@parameter1.com>
2022-01-31 17:14:34 -0600

    merge SDC configs/templates

--- tenants/scn/config/email-x.js
+++ tenants/scn/config/email-x.js
@@ -30,5 +30,33 @@ config
       width: 600,
       height: 100,
     },
+  ])
+  .setAdUnits('headline-news', [
+    {
+      name: 'banner-1',
+      id: '5c51c2469bcbcf1119dc9816',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'banner-2',
+      id: '5c51c28e58ff6944ce55d111',
+      width: 600,
+      height: 100,
+    },
+  ])
+  .setAdUnits('show-report-sdce', [
+    {
+      name: 'banner-1',
+      id: '61d4a04e7c9802f6e467c85b',
+      width: 600,
+      height: 100,
+    },
+    {
+      name: 'banner-2',
+      id: '61d4a06801e4b32a5e5d54b4',
+      width: 600,
+      height: 100,
+    },
   ]);
 module.exports = config;
